7f22afc079d949b3022cdd28309bac271cb2dc95
Daniel <gallagher_dan@yahoo.co.uk>
2018-11-20 14:54:46 +0000

    half done the blog, it'll do

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# Add the blog app to settings
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to="images")
+# Create a migration
